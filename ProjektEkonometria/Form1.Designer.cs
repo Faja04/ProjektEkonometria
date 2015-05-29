6deeb5d1c424f72dd4ff014bd2b5fa45e88cb28d
Faja04 <roland.f16@gmail.com>
2015-05-29 16:45:58 +0200

    pobieranie danych z excela

--- ProjektEkonometria/Form1.Designer.cs
+++ ProjektEkonometria/Form1.Designer.cs
@@ -28,12 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.getExelY = new System.Windows.Forms.Button();
+            this.getExelX = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.X1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(338, 225);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(356, 23);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(137, 225);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // getExelY
+            // 
+            this.getExelY.Location = new System.Drawing.Point(356, 254);
+            this.getExelY.Name = "getExelY";
+            this.getExelY.Size = new System.Drawing.Size(137, 23);
+            this.getExelY.TabIndex = 3;
+            this.getExelY.Text = "Pobierz dane Y";
+            this.getExelY.UseVisualStyleBackColor = true;
+            this.getExelY.Click += new System.EventHandler(this.getExelY_Click);
+            // 
+            // getExelX
+            // 
+            this.getExelX.Location = new System.Drawing.Point(12, 254);
+            this.getExelX.Name = "getExelX";
+            this.getExelX.Size = new System.Drawing.Size(338, 23);
+            this.getExelX.TabIndex = 4;
+            this.getExelX.Text = "Pobierz dane X";
+            this.getExelX.UseVisualStyleBackColor = true;
+            this.getExelX.Click += new System.EventHandler(this.getExelX_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pobieranie danych (plik Excel musi być otwrty)";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X1,
+            this.X2,
+            this.X3,
+            this.X4});
+            this.dataGridView3.Location = new System.Drawing.Point(12, 319);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.Size = new System.Drawing.Size(338, 204);
+            this.dataGridView3.TabIndex = 6;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Y1});
+            this.dataGridView4.Location = new System.Drawing.Point(356, 319);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.ReadOnly = true;
+            this.dataGridView4.Size = new System.Drawing.Size(137, 204);
+            this.dataGridView4.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Location = new System.Drawing.Point(9, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Współczynnik korelacji";
+            // 
+            // X1
+            // 
+            this.X1.HeaderText = "X1";
+            this.X1.Name = "X1";
+            this.X1.ReadOnly = true;
+            // 
+            // X2
+            // 
+            this.X2.HeaderText = "X2";
+            this.X2.Name = "X2";
+            this.X2.ReadOnly = true;
+            // 
+            // X3
+            // 
+            this.X3.HeaderText = "X3";
+            this.X3.Name = "X3";
+            this.X3.ReadOnly = true;
+            // 
+            // X4
+            // 
+            this.X4.HeaderText = "X4";
+            this.X4.Name = "X4";
+            this.X4.ReadOnly = true;
+            // 
+            // Y1
+            // 
+            this.Y1.HeaderText = "Y1";
+            this.Y1.Name = "Y1";
+            this.Y1.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Oblicz współczynnik korelacji";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 532);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getExelX);
+            this.Controls.Add(this.getExelY);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button getExelY;
+        private System.Windows.Forms.Button getExelX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y1;
+        private System.Windows.Forms.Button button1;
+
+
     }
 }
 
--- ProjektEkonometria/Form1.cs
+++ ProjektEkonometria/Form1.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,87 @@ namespace ProjektEkonometria
 {
     public partial class Form1 : Form
     {
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        OleDbDataAdapter adapter = new OleDbDataAdapter();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+        
+        private void getExelY_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path = Path.Combine(path, "daneY.ods");
+            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path +
+                                      @";Extended Properties=""Excel 8.0;HDR=YES;""";
+            OleDbConnection conn = new OleDbConnection(connectionString);
+            string strCmd = "select * from [Arkusz1$A1:A27]";
+            OleDbCommand cmd = new OleDbCommand(strCmd, conn);
+            try
+            {
+                conn.Open();
+                ds2.Clear();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(ds2);
+                dataGridView2.DataSource = ds2.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void getExelX_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path = Path.Combine(path, "daneX.ods");
+            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path +
+                                      @";Extended Properties=""Excel 8.0;HDR=YES;""";
+            OleDbConnection conn = new OleDbConnection(connectionString);
+            string strCmd = "select * from [Arkusz1$A1:D27]";
+            OleDbCommand cmd = new OleDbCommand(strCmd,conn);
+            try
+            {
+                conn.Open();
+                ds.Clear();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
     }
 }
